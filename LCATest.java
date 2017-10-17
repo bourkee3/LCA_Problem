d7c45b11e84013b0a97e066514d99406b6b62ce0
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-10-17 23:05:25 +0100

    First Test Commit
    Initial tests commited, testing when binary tree is empty and testing empty tree assert Null

--- LCATest.java
+++ LCATest.java
@@ -2,11 +2,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LCATest {
+public class LCATest{
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyTree() {
+		
+		LCA tree = new LCA();
+		assertNull("Find the Lowest Common Ancestor when the Binary Tree is empty: ", tree.findLCA(1, 2));
+		assertEquals("Find the Lowest Common Ancestor when the Binary Tree is empty: ", null, tree.findLCA(1, 2));
 	}
+	
+	@Test
+	public void testNullCase()
+	{
+		LCA tree = new LCA();
+		
+		assertNull("Test method returns null for an empty tree", tree.findLCA(1,2));
+	}
+
+
+	
+	
+
+
 
 }
