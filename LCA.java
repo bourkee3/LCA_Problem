8d089b0e5b089c9eaf3af94a7eb9fc407cc9800d
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-10-17 22:22:08 +0100

    Class to find LCA
    Method to find the lowest common ancestor when given two nodes n1 and n2 using a root

--- LCA.java
+++ LCA.java
@@ -1,21 +1,49 @@
 
 public class LCA {
-	
+
 	public class TreeNode{
 		int data;
 		TreeNode left, right;
-		
+
 		public TreeNode(int item){
-			
+
 			data = item;
-			
+
 			left = null;
-			
+
 			left = right; 
 		}
 	}
 
-	
-	
+	public class BinaryTree {
+		
+		TreeNode root;
+		TreeNode findLCA( int n1, int n2){
+			return findLCA(root, n1, n2);
+		}
+
+		TreeNode findLCA(TreeNode n, int n1, int n2){
+
+			if ( n == null){
+				return null;
+			}
+			if( n.data == n1 || n.data == n2){
+				return n;
+			}
+
+			TreeNode leftLCA = findLCA(n.left, n1, n2);
+			TreeNode rightLCA = findLCA(n.right, n1,n2);
+			
+			if( leftLCA != null && rightLCA != null){
+				return n;
+			}return (leftLCA != null) ? leftLCA : rightLCA;
+			
+		}
+
+
+
+	}
+
+
 
 }
