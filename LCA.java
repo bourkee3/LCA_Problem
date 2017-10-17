5d8f0a451c8f87b02158e05d090120b93470b8e1
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-10-17 22:13:48 +0100

    Create binary tree node
    A class to initilise the node, create the variable data and TreeNode left and right for the binary tree

--- LCA.java
+++ LCA.java
@@ -1,4 +1,21 @@
 
 public class LCA {
+	
+	public class TreeNode{
+		int data;
+		TreeNode left, right;
+		
+		public TreeNode(int item){
+			
+			data = item;
+			
+			left = null;
+			
+			left = right; 
+		}
+	}
+
+	
+	
 
 }
