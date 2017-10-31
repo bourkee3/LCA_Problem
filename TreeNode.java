35cf4f95f8a5919762e3bd88ba0564ad411ebcd5
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-10-31 16:44:05 +0000

    New Node Class
    Created a seperate Node class to use in DAG lowest common ancestor class aswell

---
+++ TreeNode.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class TreeNode{
+	
+	int data;
+	TreeNode left, right;
+	ArrayList <TreeNode> ancestors;
+
+	TreeNode(int item){
+
+		data = item;
+		left = right;
+		left = null;
+		right = null;
+		ancestors = null;
+		
+	
+	}
+	
+	
+}
