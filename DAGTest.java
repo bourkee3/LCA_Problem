a6d83260eeb620b1a2e6c90894a29dd0e911c97a
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-10-31 22:39:16 +0000

    Tests for DAG implementation

--- DAGTest.java
+++ DAGTest.java
@@ -5,8 +5,21 @@ import org.junit.Test;
 public class DAGTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSizeEmpty() {
+		DAG treeEmpty = new DAG();
+
+		assertEquals("Testing the lowest common ancestor of empty tree", -1,
+				treeEmpty.LCA(0, 0));
 	}
+
+	@Test
+	public void testSizeOneNode() {
+		DAG treeOneNode = new DAG();
+		treeOneNode.rootNode = new TreeNode(1);
+
+		assertEquals("Testing the lowest common ancestor of a one node tree", -1,
+				treeOneNode.LCA(1, 0));
+	}
+
 
 }
