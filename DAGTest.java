e6746ef864e698ffa56776d6dfe9babcdce8a89f
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-11-01 12:50:43 +0000

    Initial BT test
    In the test I created a binary tree of 15 nodes and then tested the LCA method

--- DAGTest.java
+++ DAGTest.java
@@ -24,7 +24,54 @@ public class DAGTest {
 	//Create new Directed Acyclic Graph to use in tests
 	DAG newTree = new DAG();
 	
-	
+	@Test
+	public void test15NodeTree() {
+		
+		//creating tree with 15 nodes 
+		
+		newTree.rootNode = new TreeNode(1);
+		newTree.rootNode.left = new TreeNode(2);
+		newTree.rootNode.right = new TreeNode(3);
+		newTree.rootNode.left.left = new TreeNode(4);
+		newTree.rootNode.left.right = new TreeNode(5);
+		newTree.rootNode.right.left = new TreeNode(6);
+		newTree.rootNode.right.right = new TreeNode(7);
+		newTree.rootNode.left.left.left = new TreeNode(8);
+		newTree.rootNode.left.left.right = new TreeNode(9);
+		newTree.rootNode.left.right.left = new TreeNode(10);
+		newTree.rootNode.left.right.right = new TreeNode(11);
+		newTree.rootNode.right.left.left = new TreeNode(12);
+		newTree.rootNode.right.left.right = new TreeNode(13);
+		newTree.rootNode.right.right.left = new TreeNode(14);
+		newTree.rootNode.right.right.right = new TreeNode(15); 
+
+		assertEquals("LCA of this tree is", 1,
+				newTree.LCA(1, 2));
+		assertEquals("LCA of this tree is", 1,
+				newTree.LCA(2, 3));
+		assertEquals("LCA of this tree is", 2,
+				newTree.LCA(2, 4));
+		assertEquals("LCA of this tree is", 2,
+				newTree.LCA(4, 5));
+		assertEquals("LCA of this tree is", 3,
+				newTree.LCA(6, 7));
+		assertEquals("LCA of this tree is", 4,
+				newTree.LCA(4, 8));
+		assertEquals("LCA of this tree is", 4,
+				newTree.LCA(8, 9));
+		assertEquals("LCA of this tree is", 6,
+				newTree.LCA(6, 12));
+		assertEquals("LCA of this tree is", 7,
+				newTree.LCA(14, 15));
+		assertEquals("LCA of this tree is", 2,
+				newTree.LCA(8, 2));
+		assertEquals("LCA of this tree is", 2,
+				newTree.LCA(9, 11));
+		assertEquals("LCA of this tree is", 3,
+				newTree.LCA(13, 14));
+		assertEquals("LCA of this tree is", 1,
+				newTree.LCA(15, 1));
+	}
 
 
 }
